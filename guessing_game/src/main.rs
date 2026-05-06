e37c8b1bdc3c2c6be6625e8aaa2aabb325419959
han-nwin <hannguyen.win@gmail.com>
2026-05-06 10:49:08 -0500

    add loop for guessing

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,33 +10,34 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new(); // mutable var
+        let mut guess = String::new(); // mutable var
 
-    // Read user input
-    // Result's variants are Ok and Err. If Ok -> take the output of Ok
-    // if Err -> need to handle error
-    // The right way to suppress the warning 
-    // is to actually write error-handling code,
-    // but in our case we just want to crash this program when a problem occurs, 
-    // so we can use expect
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // Read user input
+        // Result's variants are Ok and Err. If Ok -> take the output of Ok
+        // if Err -> need to handle error
+        // The right way to suppress the warning
+        // is to actually write error-handling code,
+        // but in our case we just want to crash this program when a problem occurs,
+        // so we can use expect
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // type conversion to unsigned 32-bit number
-    // Rust allows us to shadow the previous value of
-    // guess with a new one. Shadowing lets us reuse the guess variable
-    // name rather than forcing us to create two unique variables
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // type conversion to unsigned 32-bit number
+        // Rust allows us to shadow the previous value of
+        // guess with a new one. Shadowing lets us reuse the guess variable
+        // name rather than forcing us to create two unique variables
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => println!("You win!"),
+            Ordering::Greater => println!("Too big"),
+        }
     }
-
 }
