c3df900e3ca39198cb430ea951f9d490de8df1aa
han-nwin <hannguyen.win@gmail.com>
2026-05-10 18:32:52 -0500

    reference

--- 4.ownership/reference/src/main.rs
+++ 4.ownership/reference/src/main.rs
@@ -1,3 +1,51 @@
+// A reference is a a pointer pointing to the address on
+// the stack and read its' value (can either be on stack or heap)
+// Stack:
+//
+// s
+// ┌────────────────────┐
+// │ ptr ─────────────┐ │
+// │ len = 5          │ │
+// │ cap = 5          │ │
+// └────────────────────┘ │
+//                        │
+// Heap:                  │
+// ┌────────────────────┐ │
+// │ h e l l o          │◄┘
+// └────────────────────┘
+//
+// r
+// ┌────────────────────┐
+// │ address of s       │
+// └────────────────────┘
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+
+    // let s = String::from("hello");
+    // change1(&s);
+
+    let mut s = String::from("hello");
+    change(&mut s);
+    println!("{s}");
+}
+
+fn calculate_length(s: &String) -> usize {
+    // s is a reference to a String
+    s.len()
+} // Here, s goes out of scope. But because s does not have ownership of what
+// it refers to, the String is not dropped.
+
+// This will throw error if we tryna modify from a referene
+// fn change1(some_string: &String) {
+//     some_string.push_str(", world");
+// }
+
+// Make it a mutable reference then we can modify
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
