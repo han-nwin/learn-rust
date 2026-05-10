500959e3e02f8ecdf5d25ebc920313652f6a846d
han-nwin <hannguyen.win@gmail.com>
2026-05-09 23:28:22 -0500

    ownership

--- 4.ownership/ownership/src/main.rs
+++ 4.ownership/ownership/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
-}
+    // The String Type
+    let mut s = String::from("hello"); // create a string from a string literal
+
+    s.push_str(", world!"); // push_str() appends a literal to a String
+
+    println!("{s}"); // this will print `hello, world!`
+} // this scope is now over, and s is no longer valid. drop() is called
