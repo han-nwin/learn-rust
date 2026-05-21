3723589e97903478d4bc6ead4bcbf1bea5ae7091
han-nwin <hannguyen.win@gmail.com>
2026-05-21 13:20:44 -0500

    loop

--- 8.Common-collections/vector/src/main.rs
+++ 8.Common-collections/vector/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
 
     v.push(2);
     v.push(3);
+    v.push(5);
 
     println!("{:?}", v);
     println!("{:?}", v2);
@@ -20,16 +21,31 @@ fn main() {
         None => println!("There is no third element."),
     }
 
-    let does_not_exist = &v[100]; // THIS WILL PANIC
-    let does_not_exist = v.get(100); // THIS RETURNS NONE
+    // let does_not_exist = &v[100]; // THIS WILL PANIC
+    // let does_not_exist = v.get(100); // THIS RETURNS NONE
     //
-    //
-    let mut v = vec![1, 2, 3, 4, 5];
-
-    let first = &v[0];
-
-    v.push(6); // mem realloc happens here
+    let mut v5 = vec![1, 2, 3, 4, 5];
+    let first = &v5[0];
+    // v.push(6); // mem realloc happens here
     // first variable (reference to the original vec) no longer valid
 
     println!("The first element is: {first}");
+
+    // Iterating over values in vector
+    let v3 = vec![100, 32, 57];
+    for i in &v3 {
+        println!("{i}");
+    }
+
+    // Updating
+    let mut v = vec![100, 23, 44];
+    for i in &mut v {
+        *i += 50; // dereference the pointer
+        println!("{i}")
+    }
+    // or
+    for index in 0..v.len() {
+        v[index] += 50;
+        println!("{}", v[index]);
+    }
 }
