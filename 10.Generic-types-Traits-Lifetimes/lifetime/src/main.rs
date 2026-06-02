317aac070ad06bcebd1f921a79bb5c6eb390e58b
han-nwin <hannguyen.win@gmail.com>
2026-06-02 15:18:16 -0500

    lifetime

--- 10.Generic-types-Traits-Lifetimes/lifetime/src/main.rs
+++ 10.Generic-types-Traits-Lifetimes/lifetime/src/main.rs
@@ -35,15 +35,27 @@ fn main2() {
 // here a reference a memoery of b but b dies after a => ACCEPTED
 
 fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
 
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {result}");
+        println!("The longest string is {result}"); // this works
+    }
+    // println!("The longest string is {result}"); // this won't. since string2 already out of scope
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     // Rust doesn't know the lifetime of returned &str
     // so need to use 'a to tell
     if x.len() > y.len() { x } else { y }
+} // NOTE: 'a is just a generic for reference (like T) -> hence the <'a>
+//
+
+//Relationship
+fn longest_2<'a>(x: &str, y: &str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str()
 }
+// return lifetime doesn't know about y -> if y go out of scope it'll fail
