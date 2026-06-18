f3d2cedaf41ff897ff8ba3ca8d7f9de3e87e269c
han-nwin <hannguyen.win@gmail.com>
2026-06-18 17:12:29 -0500

    12 IO prog in progress

---
+++ 12.IO-CLI-Project/minigrep/src/main.rs
@@ -0,0 +1,51 @@
+use minigrep::search;
+use std::env;
+use std::error::Error;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    println!("--------------");
+
+    if let Err(err) = run(config) {
+        println!("Run error: {err}");
+        process::exit(1);
+    }
+}
+
+struct Config<'a> {
+    query: &'a str,
+    file_path: &'a str,
+}
+
+impl<'a> Config<'a> {
+    //the returned Config contains string
+    // slices borrowed from args, and it cannot outlive
+    // args.
+    fn build(args: &'a [String]) -> Result<Config<'a>, &'static str> {
+        if args.len() < 3 {
+            return Err("Usage: minigrep <query> <file>");
+        }
+        let query = &args[1];
+        let file_path = &args[2];
+
+        Ok(Config { query, file_path })
+    }
+}
+
+// dyn = dynamic dispatch. aka any implementation of error
+// box -> since dyn error is unknown size at compile time
+// -> box put it on the heap and give us a pointer
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("{}", contents);
+    Ok(())
+}
