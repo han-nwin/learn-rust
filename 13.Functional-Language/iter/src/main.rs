595e34badafa2cf7977f1a7b54ab6c8ae7bdd6e5
han-nwin <hannguyen.win@gmail.com>
2026-06-21 15:24:37 -0500

    iter

--- 13.Functional-Language/iter/src/main.rs
+++ 13.Functional-Language/iter/src/main.rs
@@ -5,6 +5,12 @@ fn main() {
     for val in vec_iter {
         println!("Got {val}");
     }
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+    // collect consume the iterator and create
+    // new vector
 }
 
 // Iterator trait
